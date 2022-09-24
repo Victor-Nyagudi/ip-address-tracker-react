20bf0f147323bb37d3f14825b0d83435e95f1b33
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-09-24 11:17:07 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './main.css';
+import arrowIcon from './img/icon-arrow.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="header container">
+          <h1 className="header__title">IP Address Tracker</h1>
+          
+          <form role="search">
+            <input 
+              type="search" 
+              name="searchInput" 
+              id="searchInput" 
+              placeholder="Search for any IP address or domain" 
+              className="header__search" 
+              aria-label="Search for an IP address" 
+              autoFocus
+            />
+            
+            <input 
+              type="image" 
+              src={ arrowIcon } 
+              alt="Submit" 
+              className="header__submit"
+            />
+          </form>
+        </div>
+      
+        <section className="results container">
+          <div className="results__section">
+            <h2 className="results__title">
+              IP Address
+            </h2>
+            
+            <p className="results__result ip-address">
+            </p>
+          </div>
+          
+          <div className="results__section">
+            <h2 className="results__title">
+              Location
+            </h2>
+            
+            <p className="results__result location">
+            </p>
+          </div>
+          
+          <div className="results__section">
+            <h2 className="results__title">
+              Timezone
+            </h2>
+            
+            <p className="results__result">
+              UTC <span className="timezone"></span>
+            </p>
+          </div>
+          
+          <div className="results__section">
+            <h2 className="results__title">
+              ISP
+            </h2>
+            
+            <p className="results__result results__result--isp isp">
+            </p>
+          </div>
+        </section>    
       </header>
-    </div>
+      
+      <main id="mapId2" className="mapId2" aria-live="assertive">
+        <h2 className="screen-reader-only">Map showing your IP address location</h2>
+      </main>
+    </>
   );
 }
 
